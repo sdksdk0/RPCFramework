5c893d9dffd620d8b915e1b60ab766f755258651
sdksdk0 <690644511@qq.com>
2019-06-28 10:43:03 +0800

    增加netty聊天系统的功能

---
+++ netty_chat/src/main/java/cn/tf/netty/server/handler/TerminalServerHandler.java
@@ -0,0 +1,30 @@
+package cn.tf.netty.server.handler;
+
+import cn.tf.netty.processor.MsgProcessor;
+import cn.tf.netty.protocol.IMMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TerminalServerHandler extends SimpleChannelInboundHandler<IMMessage> {
+
+    private MsgProcessor processor = new MsgProcessor();
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, IMMessage msg) throws Exception {
+        processor.sendMsg(ctx.channel(), msg);
+    }
+
+    /**
+     * 异常处理
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.info("Socket Client: 与客户端断开连接:" + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
